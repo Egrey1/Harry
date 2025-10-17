232ad75a42ccb8dec771a873bf72c3d8a8dc5c60
Nateismia <Nathan.crewdson1@gmail.com>
2025-10-17 14:42:37 -0400

    change nickname callback update

--- admin commands/library/callbacks/changenicknamecallback.py
+++ admin commands/library/callbacks/changenicknamecallback.py
@@ -1,7 +1,14 @@
-from ..modules import Interaction
+from ..modules import Interaction, give_country
 
 class ChangeNickname:
-    async def change_nickname_callback(interaction: Interaction):
-    
+    async def change_nickname_callback(interaction: Interaction) -> None:
+        #item = ''.join(interaction.data['values']) 
+        #country = await give_country(interaction.user.mention)
+
+        #if not country:
+        #    await interaction.response.edit_message(content= 'Вы не зарегистрированы!', embed= None)
+        #    return None
+        
+
         pass
         
