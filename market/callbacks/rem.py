69e084f5ab0b614227c66d24ff69798b000786ab
Egrey <egrerius1.gmail.com>
2025-10-17 21:19:48 +0300

    !market_rem fix

--- market/callbacks/rem.py
+++ market/callbacks/rem.py
@@ -5,11 +5,25 @@ async def rem(interaction: Interaction, country: str):
 
     connect = con(DATABASE_COUNTRIES)
     cursor = connect.cursor()
+
+    cursor.execute(f"""
+                   SELECT `{item}`
+                   FROM market
+                   WHERE name = {country}
+                   """)
+    have = (cursor.fetchone()[0]).split()[0]
+
     cursor.execute(f"""
                     UPDATE market
                     SET `{item}` = 0
-                    WHERE `name` = ?
+                    WHERE name = ?
                     """, (country,))
+    
+    cursor.execute(f"""
+                   UPDATE countries_inventory
+                   SET `{item}` = `{item}` + {have}
+                   WHERE name = '{country}'
+                   """)
     connect.commit()
     connect.close()
     await interaction.response.send_message(f'Позиция `{item}` успешно удалена с рынка!', ephemeral=True)
